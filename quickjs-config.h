dfb0c999c4025c72644ebe604e09bdb06c0e971c
Brian Bagdasarian <brian@remodl.ai>
2026-01-31 02:56:05 -0500

    feat: Add quickjs-config.h for qjs-modules compatibility

---
+++ quickjs-config.h
@@ -0,0 +1,8 @@
+/* QuickJS-ng configuration header */
+#ifndef QUICKJS_CONFIG_H
+#define QUICKJS_CONFIG_H
+
+/* QuickJS-ng does not have CONFIG_BIGNUM */
+/* Use native BigInt instead */
+
+#endif /* QUICKJS_CONFIG_H */
